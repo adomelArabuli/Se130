37e3af734a36e1221d8225047dc24d5b9264124f
a.arabuli <a.arabuli@AARABULI-LP>
2024-01-26 21:37:00 +0400

    slider

---
+++ PasswordBox/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+using System.Windows;
+
+namespace PasswordBox
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+
+    class Person
+    {
+        public string Name { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [JsonIgnore]
+        public string Token { get; set; }
+
+        [JsonIgnore]
+        public string RefreshToken { get; set; }
+    }
+}

ec390329430126f19e00f0fee2e8debb411189b1
a.arabuli <a.arabuli@AARABULI-LP>
2024-02-07 21:21:04 +0400

    Events

---
+++ EventExampleCloseToReality/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace EventExampleCloseToReality
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            YoutubeChannel channel = new YoutubeChannel();
+            Subscriber subscriber = new Subscriber();
+            subscriber.Subscribe(channel);
+            channel.UploadVideo("Test video","https://Yotube.com/channel/TestVideo");
+            subscriber.UnSubscribe(channel);
+            channel.UploadVideo("Test video2","https://Yotube.com/channel/TestVideo2");
+        }
+    }
+
+    public class YoutubeChannel
+    {
+        public event Action<string, string> VideoUploadEvent;
+
+        protected virtual void OnVideoUpload(string videoTitle, string path)
+        {
+            VideoUploadEvent?.Invoke(videoTitle, path);
+        } 
+
+        public void UploadVideo(string videoTitle, string path)
+        {
+            OnVideoUpload(videoTitle, path);
+        }
+    }
+
+    public class Subscriber
+    {
+        public void VideoUploadHandler1(string videoTiTle, string path)
+        {
+            Console.WriteLine($"Viewer: Video alert! title: {videoTiTle} check out link here {path}");
+        }
+
+        public void VideoUploadHandler2(string videoTiTle, string path)
+        {
+            string filePath = "C:\\Users\\a.arabuli\\Desktop\\txt.txt";
+
+            using(FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate))
+            {
+                using(StreamWriter sw = new StreamWriter(fs))
+                {
+                    sw.WriteLine($"I want to log the url of the new video {path}");
+                }
+            }
+        }
+
+        public void VideoUploadHandler3(string videoTiTle, string path)
+        {
+            string filePath = "C:\\Users\\a.arabuli\\Desktop\\txt.txt";
+            string content = string.Empty;
+
+            using (FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    content = sr.ReadToEnd();
+                    Console.WriteLine(content);
+                }
+            }
+        }
+
+        public void Subscribe(YoutubeChannel channel)
+        {
+            channel.VideoUploadEvent += VideoUploadHandler1;
+            channel.VideoUploadEvent += VideoUploadHandler2;
+            channel.VideoUploadEvent += VideoUploadHandler3;
+        }
+
+        public void UnSubscribe(YoutubeChannel channel)
+        {
+            channel.VideoUploadEvent -= VideoUploadHandler1;
+            channel.VideoUploadEvent -= VideoUploadHandler2;
+            channel.VideoUploadEvent -= VideoUploadHandler2;
+        }
+    }
+}
---
+++ Review/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Review
+{
+    internal class Program
+    {
+        delegate List<string> TestDelegate(List<string> names, string filter);
+        static void Main(string[] args)
+        {
+            TestDelegate testDelegate = new TestDelegate(Test);
+            List<string> names = new List<string>()
+            {
+                "Mate","Giorgi","Gela","Marina","Tamazi","Giorgi","Mariami","Ani","Mate","Giorgi",
+            };
+            // func(in params, out bool) action(in params)
+
+            var name = names.Where(x => x.Contains("rgi"));
+
+            //var result = Test(names, "rgi");
+            var result = testDelegate(names, "rgi");
+        }
+
+        static List<string> Test(List<string> names, string filter)
+        {
+            List<string> resultNames = new List<string>(); 
+            string firstName = null;
+            foreach (var name in names)
+            {
+                if (name.Contains(filter))
+                {
+                    resultNames.Add(name);
+                }
+            }
+            return resultNames;
+        }
+    }
+}
